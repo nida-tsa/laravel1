620c291cd2ae4ebe033adb1224c252cb6cce0b09
nida-tsa <nidawaja.tsa@gmail.com>
2020-10-19 11:28:55 -0700

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class UserController extends Controller
+{
+    //
+    public function show($email){
+        // $users = .\DB::table('users')=>where('email',$email)=>first();
+    }
+}
